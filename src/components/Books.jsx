08fe36683f3dd9ac8e3a0dcce3366795b4f8c975
Habugabu <115887891+Habugabu@users.noreply.github.com>
2025-02-19 19:12:39 +0900

    8.19 completed

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,8 +1,11 @@
 import { useQuery } from "@apollo/client";
+import { useState } from "react";
 import { ALL_BOOKS } from "../queries";
 
 const Books = (props) => {
   const result = useQuery(ALL_BOOKS);
+
+  const [genreFilter, setGenreFilter] = useState("");
 
   if (!props.show) {
     return null;
@@ -13,6 +16,22 @@ const Books = (props) => {
   }
 
   const books = [...result.data.allBooks];
+
+  let genres = [];
+
+  books.forEach((b) => (genres = genres.concat(b.genres)));
+
+  genres = [...new Set(genres)];
+
+  const filteredBooks = books.filter((b) => b.genres.includes(genreFilter));
+
+  let shownBooks = [];
+
+  if (filteredBooks.length === 0) {
+    shownBooks = books;
+  } else {
+    shownBooks = filteredBooks;
+  }
 
   return (
     <div>
@@ -25,7 +44,7 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
+          {shownBooks.map((a) => (
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
@@ -34,6 +53,15 @@ const Books = (props) => {
           ))}
         </tbody>
       </table>
+      <select
+        value={genreFilter}
+        onChange={({ target }) => setGenreFilter(target.value)}
+      >
+        <option>(select a genre)</option>
+        {genres.map((a) => (
+          <option key={a}>{a}</option>
+        ))}
+      </select>
     </div>
   );
 };
