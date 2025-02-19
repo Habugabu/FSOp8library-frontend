185b5170ece6d56c306b7365b9f23271a97f91e5
Habugabu <115887891+Habugabu@users.noreply.github.com>
2025-02-19 19:33:02 +0900

    8.20 completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Authors from "./components/Authors";
 import Books from "./components/Books";
 import NewBook from "./components/NewBook";
 import Login from "./components/Login";
+import Recommend from "./components/Recommend";
 
 const App = () => {
   const [page, setPage] = useState("authors");
@@ -29,6 +30,7 @@ const App = () => {
         {token ? (
           <div>
             <button onClick={() => setPage("add")}>add book</button>
+            <button onClick={() => setPage("recommend")}>recommend</button>
             <button onClick={logout}>logout</button>
           </div>
         ) : null}
@@ -41,6 +43,8 @@ const App = () => {
       <NewBook show={page === "add"} token={token} />
 
       <Login show={page === "login"} setToken={setToken} />
+
+      <Recommend show={page === "recommend"} />
     </div>
   );
 };
