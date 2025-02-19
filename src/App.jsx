538769b7d247835c92f19752e21397cd3744bf72
Habugabu <115887891+Habugabu@users.noreply.github.com>
2025-02-19 17:42:28 +0900

    8.17 completed

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import NewBook from "./components/NewBook";
 
 const App = () => {
   const [page, setPage] = useState("authors");
+  const [token, setToken] = useState(null);
 
   return (
     <div>
